059092ff072ab55a7a85365f54f85fd3d7e5609e
tsi-mohan <mohan.tsi.developer@gmail.com>
2022-06-14 16:20:48 +0530

    Update core.cljs

--- src/myexample/core.cljs
+++ src/myexample/core.cljs
@@ -22,7 +22,7 @@
     (.redirect res (str "http://" (.get req "Host") (.-url req)))
     (go
       (.set res "Content-Type" "text/html")
-      (.sendFile res [__dirname + "pages/hello.html"]))))
+      (.sendFile res [.__dirname"/pages/hello.html"]))))
 
 (defn server [port success]
   (doto (express)
